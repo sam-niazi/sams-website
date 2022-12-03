ac5a2e09a13210dcfd36a2a7d995e6e7bf5f0ed4
Nikolas Karinja <nik@lotechstudios.com>
2022-12-03 12:40:34 -0800

    Cool...

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require( 'express' )
-const http = require( 'http' )
+const express  = require( 'express' )
+const http     = require( 'http' )
 const socketio = require( 'socket.io' )
 
 const Server = new http.Server( express() )
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const response = await fetch( '../src/caps.json' ) 
-const Hats = await response.json()
+const Hats     = await response.json()
 
 const Body = document.body
 
@@ -7,18 +7,18 @@ for ( let i = 0; i < Hats.hats.length; i++ ) {
 
     const Hat = Hats.hats[ i ]
 
-    const HatElement = document.createElement( 'hat' )
-    const HatNameElement = document.createElement( 'hat-name' )
+    const HatElement      = document.createElement( 'hat' )
+    const HatNameElement  = document.createElement( 'hat-name' )
     const HatImageElement = document.createElement( 'img' )
-    const HatDescElement = document.createElement( 'hat-desc' )
+    const HatDescElement  = document.createElement( 'hat-desc' )
     const HatPriceElement = document.createElement( 'hat-price' )
 
-    HatElement.style.animation = `fade-in 1.5s ease-out forwards`
+    HatElement.style.animation      = `fade-in 1.5s ease-out forwards`
     HatElement.style.animationDelay = `${ i }s`
 
     HatNameElement.innerHTML = Hat.name
 
-    HatImageElement.src = Hat.image
+    HatImageElement.src   = Hat.image
     HatImageElement.width = '512'
 
     HatDescElement.innerHTML = Hat.description
