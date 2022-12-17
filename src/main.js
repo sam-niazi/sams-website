4fd56045448957e129becb23e980e85e1271ece3
Nikolas Karinja <nik@lotechstudios.com>
2022-12-17 12:12:05 -0800

    asd

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 const response = await fetch( '../src/caps.json' ) 
 const Hats     = await response.json()
 
+function preload() {
+
+    const images = []
+
+    for ( var i = 0; i < arguments.length; i++ ) {
+
+        images[ i ]     = new Image()
+        images[ i ].src = arguments[ i ]
+
+    }
+}
+
+preload(
+    "https://www.howardstern.com/wp-content/uploads/sites/3/2022/09/mens-new-era-red-kansas-city-chiefs-2022-sideline-9fifty-ink-dye-snapback-hat_ss5_p-4687652pv-3u-1d1vf4jjjog1q2cjrzj5v-pbvsabfwbr9f2079o7gd.jpg?crop=6px%2C149px%2C900px%2C599px&resize=700%2C466&quality=100",
+    "https://fanatics.frgimages.com/minnesota-vikings/mens-new-era-purple-minnesota-vikings-omaha-59fifty-fitted-hat_pi2539000_ff_2539493_full.jpg?_hv=2&w=900",
+    "https://images.footballfanatics.com/cincinnati-bengals/mens-new-era-olive-cincinnati-bengals-color-pack-59fifty-fitted-hat_pi4673000_altimages_ff_4673523-883c03762b408c0bd13aalt2_full.jpg?_hv=2&w=900"
+)
+
 const Body = document.body
 
 for ( let i = 0; i < Hats.hats.length; i++ ) {
@@ -38,3 +56,4 @@ const EndElement = document.createElement( 'p' )
 EndElement.innerHTML = `This is my website. To visit Nike, click <a href='https://www.nike.com/'>this</a>. If you want to visit Lids, click <a href='https://www.lids.com/'>here</a>.`
 
 Body.appendChild( EndElement )
+
